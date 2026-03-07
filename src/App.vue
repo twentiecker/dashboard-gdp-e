e6f99c37e23faf466814e875ef7e4d24f2e72d1b
twentiecker <twentiecker@gmail.com>
2026-03-07 16:33:50 +0700

    add primevue and tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="flex flex-col gap-5 p-10">
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem explicabo
+      perferendis recusandae quod nostrum reprehenderit pariatur libero quas
+      alias porro expedita enim ipsa facere, dolores voluptatum distinctio
+      aliquid earum corrupti quam corporis doloribus cumque? Dignissimos, illo
+      quis. Maxime iste, rem obcaecati in dolorem adipisci et fuga voluptatum
+      quaerat? Iste, pariatur? Dicta ex harum quasi?
+    </p>
+    <Button label="Hello PrimeVue" icon="pi pi-check" />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
-
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,14 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import PrimeVue from "primevue/config";
+import Aura from "@primeuix/themes/aura";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+});
+
+app.mount("#app");
