5e8548777dc22a35c5a31764240da4324c5867a0
twentiecker <twentiecker@gmail.com>
2026-03-26 14:31:48 +0700

    first commit

--- src/views/app/AboutView.vue
+++ src/views/app/AboutView.vue
@@ -1,3 +1,29 @@
+<script setup>
+import { ref, onMounted } from "vue";
+import axios from "axios";
+
+const data = ref([]);
+const BASE_URL = "https://dnpeng-backend-fastapi-production.up.railway.app";
+const getPkrtData = async () => {
+  try {
+    const response = await axios.get(`${BASE_URL}/api/v1/pkrt/`, {
+      params: {
+        kode: "MB105",
+        periode: "",
+      },
+    });
+
+    data.value = response.data;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+onMounted(() => {
+  getPkrtData();
+});
+</script>
+
 <template>
   <div class="app-container">
     <h1>About View</h1>
@@ -9,5 +35,6 @@
       quis. Maxime iste, rem obcaecati in dolorem adipisci et fuga voluptatum
       quaerat? Iste, pariatur? Dicta ex harum quasi?
     </p>
+    {{ data }}
   </div>
 </template>
